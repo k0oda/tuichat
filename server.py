c8d0ac0b297df66c67f109684981421c2fbaa1e8
Kvkr3 <kulevgen32@gmail.com>
2019-04-19 19:09:50 +0600

    Add Server class object and fixed disconnection blank-lines bug

--- server.py
+++ server.py
@@ -27,24 +27,28 @@ class Server:
 
     def accept(self):
         self.conn, self.addr = sock.accept()
-        print(self.time(Server), "Подключен новый пользователь:", self.addr[0])
+        print(self.time(), "Подключен новый пользователь:", self.addr[0])
 
     def get_data(self, conn, addr,):
         while True:
             try:
                 data = conn.recv(48634).decode('utf-8')
-                print(self.time(Server), addr[0], "-", data)
-                conn.send(bytes("Server: Сообщение получено!", encoding="utf-8"))
+                if data:
+                    print(self.time(), addr[0], "-", data)
+                    conn.send(bytes("Server: Сообщение получено!", encoding="utf-8"))
             except ConnectionResetError:
-                print(self.time(Server), addr[0], "отключен!")
+                conn.close()
+                print(self.time(), addr[0], "отключен!")
                 break
             except ConnectionAbortedError:
-                print(self.time(Server), addr[0], "отключился!")
+                conn.close()
+                print(self.time(), addr[0], "отключился!")
                 break
 
 
+server = Server()
 while True:
-    connection = Thread(target=Server.accept(Server))
+    connection = Thread(target=server.accept())
     connection.start()
-    user = Thread(target=Server.get_data, args=(Server, Server.conn, Server.addr,))
+    user = Thread(target=server.get_data, args=(server.conn, server.addr,))
     user.start()
