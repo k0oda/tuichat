b483cdd1ff70a5aa47c5f1da38891a7753a6e524
Evgeniy Kuleshov <kulevgen32@gmail.com>
2019-05-16 20:52:33 +0600

    Fix exception in blank data

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from threading import Thread
 from urllib import request
 from json import loads, dumps, JSONDecodeError
 from os import system, name
+import sys
 import pychat_ui
 
 
@@ -11,19 +12,19 @@ class Server:
     connections_list = []
 
     def clear_screen(self,):
-        system("cls" if name == 'nt' else 'clear')
+        system('cls' if name == 'nt' else 'clear')
 
     def get_time(self,):
         current_time = datetime.now().strftime('%Y-%m-%d | %H:%M:%S ')
         return current_time
 
     def save_log(self, data, open_type,):
-        log_file = open("log.txt", open_type)
+        log_file = open('log.txt', open_type)
         log_file.write(data)
         log_file.close()
 
     def save_config(self, max_connections, port, enable_log, enable_ui,):
-        parameters_list = [{"max_connections": max_connections}, {"port": port}, {"enable_log": enable_log}, {"enable_ui": enable_ui}]
+        parameters_list = [{'max_connections': max_connections}, {'port': port}, {'enable_log': enable_log}, {'enable_ui': enable_ui}]
         config = open('config.json', 'w')
         parametersJSON = dumps(parameters_list)
         config.write(parametersJSON)
@@ -34,42 +35,44 @@ class Server:
                 self.connection, self.address = self.sock.accept()
                 self.connections_list.append(self.connection)
                 time = self.get_time()
-                new_user_msg = f"{time} New user connected: {self.address[0]}"
-                print(new_user_msg)
-                self.send_messages(new_user_msg + "\n")
-                get_msg = Thread(target=self.get_data, args=(self.connection, self.address,))
+                print(f'{self.get_time()} {self.address[0]} connected!')
+                new_user_msg = self.serialize_data(self.get_time(), 'connected!', self.address[0])
+                self.send_messages(new_user_msg)
+                get_msg = Thread(target=self.get_data, args=(self.connection, self.address[0]))
                 get_msg.start()
             else:
-                continue
+                self.connection, self.address = self.sock.accept()
+                full_server_error = self.serialize_data(self.get_time(), 'Server is full!', 'Server:')
+                self.connection.sendall(bytes(dumps(full_server_error)))
 
     def configure(self,):
         while True:
-            print("Would you like to configure server now? [Y/n]")
-            answer = input("> ").lower().strip()
-            if answer == "y":
+            print('Would you like to configure server now? [Y/n]')
+            answer = input('> ').lower().strip()
+            if answer == 'y':
                 try:
                     self.clear_screen()
-                    max_connections_input = int(input("Enter value for limit of connections > ").strip())
-                    port_input = int(input("Enter port > ").strip())
+                    max_connections_input = int(input('Enter value for limit of connections > ').strip())
+                    port_input = int(input('Enter port > ').strip())
 
-                    enable_log_input = input("Enable log? (Y/n) > ").lower().strip()
-                    if enable_log_input == "y":
+                    enable_log_input = input('Enable log? (Y/n) > ').lower().strip()
+                    if enable_log_input =='y':
                         enable_log_input = True
                     elif enable_log_input == 'n':
                         enable_log_input = False
                     else:
                         raise ValueError
 
-                    enable_ui_input = input("Enable UI symbols? (Y/n) > ").lower().strip()
-                    if enable_ui_input == "y":
+                    enable_ui_input = input('Enable UI symbols? (Y/n) > ').lower().strip()
+                    if enable_ui_input == 'y':
                         enable_ui_input = True
                     elif enable_ui_input == 'n':
                         enable_ui_input = False
                     else:
                         raise ValueError
 
-                    save_config_input = input("Save current settings to new configuration file? (Y/n) > ").lower().strip()
-                    if save_config_input == "y":
+                    save_config_input = input('Save current settings to new configuration file? (Y/n) > ').lower().strip()
+                    if save_config_input == 'y':
                         self.save_config(max_connections_input, port_input, enable_log_input, enable_ui_input)
                     elif save_config_input == 'n':
                         pass
@@ -78,53 +81,58 @@ class Server:
                     self.clear_screen()
                 except ValueError:
                     self.clear_screen()
-                    print("[ERROR] Error in data entry!")
+                    print('[ERROR] Error in data entry!')
                 else:
                     return max_connections_input, port_input, enable_log_input, enable_ui_input
-            elif answer == "n":
+            elif answer == 'n':
                 self.clear_screen()
-                print("Using standart variables...")
+                print('Using standart variables...')
                 return 5, 8000, False, False
             else:
                 self.clear_screen()
-                print("[ERROR] Unknown command!")
+                print('[ERROR] Unknown command!')
 
-    def get_data(self, conn, address,):
+    def get_data(self, conn, address):
         while True:
             try:
-                size = int(conn.recv(14).decode('utf-8'))
-                data = conn.recv(size).decode('utf-8')
-                data = loads(data)
+                data = conn.recv(2048).decode('utf-8')
                 if data:
-                    data = f"{data['sending_time']} {address[0]} - {data['message']}"
+                    data_dict = loads(data)
+                    data = f'{data_dict["sending_time"]} {address} - {data_dict["message"]}'
                     print(data)
-                    self.send_messages(data + "\n")
-            except ConnectionResetError:
-                conn.close()
-                self.connections_list.remove(conn)
-                connection_reset_msg = f"{self.get_time()} {address[0]} disconnected!"
-                print(connection_reset_msg)
-                self.send_messages(connection_reset_msg + "\n")
-                break
-            except ConnectionAbortedError:
-                conn.close()
-                self.connections_list.remove(conn)
-                connection_aborted_msg = f"{self.get_time()} {address[0]} disconnected!"
-                print(connection_aborted_msg)
-                self.send_messages(connection_aborted_msg + "\n")
-                break
+                    # conn.send(bytes(encoding='utf-8')) # Respond to client that server got message
+                    send_data = self.serialize_data(data_dict['sending_time'], data_dict['message'], address)
+                    self.send_messages(send_data)
+            except (ConnectionResetError, ConnectionAbortedError):
+            	conn.close()
+            	self.connections_list.remove(conn)
+            	print(f'{self.get_time()} {self.address[0]} disconnected!')
+            	connection_aborted_msg = self.serialize_data(self.get_time(), 'disconnected!', self.address[0])
+            	self.send_messages(connection_aborted_msg)
+            	break
+    
+    def serialize_data(self, sending_time, message, address):
+    	message_dict = {
+    		'sending_time': sending_time,
+    		'message': message,
+    		'sender_address': address,
+    		}
+    	serialized_dict = dumps(message_dict)
+    	return serialized_dict
 
-    def send_messages(self, message,):
+
+    def send_messages(self, message):
         if enable_log:
             self.save_log(message, 'a')
         for user in self.connections_list:
-            user.sendall(bytes(message, encoding="utf-8"))
+            message = dumps(message)
+            user.sendall(bytes(message, encoding='utf-8'))
 
     def run_server(self, max_connections, port,):
         self.sock = socket()
         self.sock.bind(("", port))
         self.sock.listen(max_connections)
-        external_ip = request.urlopen('http://ident.me').read().decode("utf-8")
+        external_ip = request.urlopen('http://ident.me').read().decode('utf-8')
         start_time = server.get_time()
         return external_ip, start_time
 
@@ -136,10 +144,10 @@ if __name__ == '__main__':
         config = open('config.json').read()
         config = loads(config)
     except FileNotFoundError:
-        print("[ERROR] Configuration file not found!")
+        print('[ERROR] Configuration file not found!')
         max_connections, port, enable_log, enable_ui = server.configure()
     except JSONDecodeError:
-        print("[ERROR] JSON decoding error!")
+        print('[ERROR] JSON decoding error!')
         max_connections, port, enable_log, enable_ui = server.configure()
     else:
         try:
@@ -150,7 +158,7 @@ if __name__ == '__main__':
             enable_ui = config[3]['enable_ui']
             enable_ui = bool(enable_ui)
         except ValueError:
-            print("[ERROR] Error in config file!")
+            print('[ERROR] Error in config file!')
             max_connections, port, enable_log, enable_ui = server.configure()
 
     logo = pychat_ui.logo.get_logo('server') if enable_ui else pychat_ui.logo.get_raw_logo('server')
