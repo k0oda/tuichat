0ea6c2d29563403e22a72d865fc72f8a232517c5
kukree <kulevgen32@gmail.com>
2019-07-09 00:35:10 +0600

    Clean __init__.py, update version to 0.6.2

--- tuichat/__init__.py
+++ tuichat/__init__.py
@@ -1,18 +1,8 @@
 from .server import Server
 from .client import Client
-from .tuichat_utils.ui import (
-    Logo,
-    ServerInfotable,
-    License,
-    ConnectionInfo
-)
 
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 __all__ = [
     'Server',
     'Client',
-    'Logo',
-    'ServerInfotable',
-    'License',
-    'ConnectionInfo'
 ]
