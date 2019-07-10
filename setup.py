c1436c8580b40fdc506c2b4d92cc50d37c249523
kukree <kulevgen32@gmail.com>
2019-07-10 15:51:00 +0600

    Add download_url to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kukree/tuichat",
+    download_url="httpsL//github.com/kukree/tuichat/tarball/master",
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
